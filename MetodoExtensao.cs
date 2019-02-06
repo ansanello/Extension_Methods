442a7c18b80b7935bf887dd152190418a17c3520
Diego Ansanello <diego.ansanello@gmail.com>
2019-02-06 14:40:20 -0200

    Contar caracteres e palavras

---
+++ MetodoExtensao.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodos_Extensao
+{
+    public static class MetodoExtensao
+    {
+        //indicado pela palavra this antes do nome do primeiro argumento do método
+        public static int ContaCaracteres(this string texto)
+        {
+            var iContador = texto.Length;
+            return iContador;
+        } 
+
+        //estende a classe string
+        public static int ContaPalavras(this string texto)
+        {
+            return texto.Split(new char[] { ' ', '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        //Metodo de extensao usando um tipo genérico 
+        public static string ToString<T>(this IEnumerable<T> enumeration)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            resultado.Append("[");
+            
+            foreach (var item in enumeration)
+            {
+                resultado.Append(item.ToString());
+                resultado.Append(", ");
+            }
+
+            if (resultado.Length > 1)
+            {
+                resultado.Remove(resultado.Length - 2, 2);
+            }
+
+            resultado.Append("]");
+            return resultado.ToString();
+        }
+
+    }
+
+    public static class IListExtensao
+    {
+        //O método de extensão AumentarValorLista pode acessar somente os elementos incluidos na interface IList
+        
+	public static void AumentarValorLista(this IList<int> lista, int quantidade)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                lista[i] += quantidade;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Metodos_Extensao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Apenas fazendo um teste !";
+
+            int NumeroDeCaracteres = frase.ContaCaracteres();
+            int NumeroDePalavras = frase.ContaPalavras();
+
+            Console.WriteLine("Na frase : " + frase);
+            Console.WriteLine("temos : ");
+            Console.WriteLine(NumeroDeCaracteres  + " caracteres ");
+            Console.WriteLine(NumeroDePalavras + " palavras ");
+
+            Console.ReadKey();
+
+            //------------------------------------------------------------------------------
+            Console.WriteLine("");
+            Console.WriteLine("Usando o método de extensão : AummentaValorLista() ");
+            Console.WriteLine("");
+            
+            List<int> numeros = new List<int> { 1, 2, 3, 4, 5 };
+            
+            Console.WriteLine(" 1  2  3  4  5") ;
+            Console.WriteLine(" Aumentando os valores 10 unidades ");
+
+            //usando o método de extensão 
+            numeros.AumentarValorLista(10);
+            foreach (int n in numeros)
+            {
+                Console.Write(n + " ");
+            }
+            Console.ReadKey();
+
+            //------------------------------------------------------------------------------
+            Console.WriteLine("");
+            Console.WriteLine("Usando o método de extensão : ToString<int>");
+            //usando o método de extensão 
+            Console.WriteLine("");
+            Console.WriteLine(numeros.ToString<int>());
+
+            Console.ReadKey();
+        }
+    }
+}
